0d2329c85dc56189247281fdf9274e10fa7530b0
ManuLG <idc.manuel@gmail.com>
2017-04-26 11:40:57 +0200

    Añadido esqueleto del main

--- Main.c
+++ Main.c
@@ -1,18 +1,33 @@
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h> //Prints
 #include <stdlib.h>
+
+#define N 2 // Número de nodos
 
 struct mensaje {
   int prioridad;
   int id_nodo;
-
-
 }
 
 
 int main(){
+	while (1) {
+		int quiero = 1;
+		int mi_ticket = max_ticket + 1;
+		for (i = 0; i < N-1; i++) {send(REQUEST, id_nodos[i], {mi_id, mi_ticket})};
+		for (i = 0; i < N-1; i++) {receive(REPLY, &id_aux)};
 
+		/*
 
+			SECCIÓN CRÍTICA
 
+		*/
 
-
-}
+		quiero = 0;
+		for (i = 0; i < num_pend; i++) { send(REPLY, id_nodos_pend[i], mi_id);}
+			
+		num_pend = 0;
+	} // Cierre while 
+} // Cierre main
