38ec09c193b67b29cf241aa548b61805b3ea2d66
ManuLG <idc.manuel@gmail.com>
2017-05-02 17:26:32 +0200

    Ahora el proceso es capaz de recibir solo los mensajes que van dirigidos a él

--- Main.c
+++ Main.c
@@ -9,73 +9,96 @@
 #define ID_NODO 2
 
 typedef struct mensaje {
-  int prioridad;
-  int id_nodo;
-} mensaje;
+      long      mtype;    // Necesario para filtrar el mensaje a recibir
+      int prioridad;
+      int id_nodo;
+  } mensaje;
 
-int id = 0;
+  int mi_id = 7;
+  int mi_ticket = 0;
+  int id_nodos_pend[N-1];
+  int num_pend = 0;
+  int quiero = 0;
+  int max_ticket = 0;
+
+  int id_cola = 0;
 
 // Prototipos de las funciones
-int sendMsg();
-int receiveMsg();
+  int sendMsg();
+  int receiveMsg();
 
-int main(){
-	while (1) {
+  int main(){
 
-		key_t key = ftok("/tmp", 123);
-		id = msgget(key, 0777 | IPC_CREAT);
+    key_t key = ftok("/tmp", 123);
+    id_cola = msgget(key, 0777 | IPC_CREAT);
 
-		printf("ID de la cola: %i", id);
+    printf("ID de la cola: %i\n", id_cola);
 
-		printf ("%i\n", sendMsg(id));
-		receiveMsg();
+    while (1) {
 
-		// Pseudocódigo
+      printf ("%i\n", sendMsg(mi_id));
 
-		/*
+      for (int i = 0; i < 10; i++) {
+        printf ("Envío %i\n", i);
+        sendMsg(i);
+    }
 
-		int quiero = 1;
-		int mi_ticket = max_ticket + 1;
-		for (i = 0; i < N-1; i++) {send(REQUEST, id_nodos[i], {mi_id, mi_ticket})};
-		for (i = 0; i < N-1; i++) {receive(REPLY, &id_aux)};
+      receiveMsg();
 
-		*/
+        // Pseudocódigo
 
-		/*
-			SECCIÓN CRÍTICA
-		*/
+        /*
 
-		/*
+        int quiero = 1;
+        int mi_ticket = max_ticket + 1;
+        for (i = 0; i < N-1; i++) {send(REQUEST, id_nodos[i], {mi_id, mi_ticket})};
+        for (i = 0; i < N-1; i++) {receive(REPLY, &id_aux)};
 
-		quiero = 0;
-		for (i = 0; i < num_pend; i++) { send(REPLY, id_nodos_pend[i], mi_id);}
-			
-		num_pend = 0;
+        */
 
-		*/
-	} // Cierre while 
+        /*
+            SECCIÓN CRÍTICA
+        */
+
+        /*
+
+        quiero = 0;
+        for (i = 0; i < num_pend; i++) { send(REPLY, id_nodos_pend[i], mi_id);}
+
+        num_pend = 0;
+
+        */
+    } // Cierre while 
 } // Cierre main
 
-int sendMsg(int id_destino) {
-	mensaje msg;
+// Pseudocodigo guia
+void procesoReceptor() {
+    int id_nodo_origen, ticket_origen;
+    if ((quiero == 1) || (ticket_origen < mi_ticket) || ((ticket_origen == mi_ticket && (id_nodo_origen < mi_id)))) {
 
-	msg.prioridad = PRIORIDAD;
-	msg.id_nodo = ID_NODO;
-	return msgsnd (id_destino, (struct msgbuf *)&msg, sizeof(msg), 0);
+    }
 }
 
+int sendMsg(int id_destino) {
+    mensaje msg;
+
+    msg.prioridad = PRIORIDAD;
+    msg.id_nodo = ID_NODO;
+    msg.mtype = id_destino; // ¿Destinatario?
+
+    return msgsnd (id_cola, (struct msgbuf *)&msg, sizeof(msg.prioridad)+sizeof(msg.id_nodo)+sizeof(msg.mtype), 0);
+} // Cierre sendMsg
+
+
+
+// receive()
 int receiveMsg() {
 
-	mensaje msg;
+    mensaje msg;
 
-	int res = msgsnd (id, (struct msgbuf *)&msg, sizeof(msg.prioridad)+sizeof(msg.id_nodo), 0);
+    int res = msgrcv (id_cola, (struct msgbuf *)&msg, sizeof(msg.prioridad)+sizeof(msg.id_nodo)+sizeof(msg.mtype), mi_id, 0);
 
-	printf ("Recibo: %i", msg.prioridad);
+    printf ("Recibo: %i\n", msg.mtype);
 
-	/*
-
-		Hacer lo que toque al recibir un mensaje
-
-	*/
-	return 4;
-}
+    return res;
+} // Cierre receiveMsg()
