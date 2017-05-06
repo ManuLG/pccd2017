430d82bb6adea430cc0ece59b55c426c27c5ab64
Xoán Carlos Cosmed Peralejo <xoancosmed@users.noreply.github.com>
2017-05-06 18:04:46 +0200

    Create Prueba6.sh

---
+++ pruebas/Prueba6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Un proceso de pagos y varios lectores intentan acceder a su sección crítica.
+
+./main 1 3 1 1 # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
+./main 2 3 4 5
+./main 3 3 5 5
+sleep 2
+
+# Un proceso de anulaciones quiere acceder a su sección crítica mientras algunos lectores todavía siguen en ella.
+# Deberá esperar a que los lectores que se están ejecutando salgan de su sección crítica.
+
+./main 1 3 2 1
