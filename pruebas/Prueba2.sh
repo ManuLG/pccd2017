9941591388c1d4c043f5e4d0ec19296ba15c47f3
Xoán Carlos Cosmed Peralejo <xoancosmed@users.noreply.github.com>
2017-05-06 16:50:09 +0200

    Create Prueba2.sh

---
+++ pruebas/Prueba2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Dos procesos de eventos quieren entrar en la sección crítica a la vez, estando en nodos diferentes.
+# Dado que estamos ante un sistema distribuido donde los lectores se pueden ejecutar concurrentemente, ambos procesos se ejecutarán concurrentemente, pues son lectores y no hay ningún escritor esperando.
+
+./main 1 2 5 1 # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
+./main 2 2 5 1
