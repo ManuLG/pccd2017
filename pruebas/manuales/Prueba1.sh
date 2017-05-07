6540d4d862a276bd8b0613a773164849437ad474
Xoán <xoancosmed@gmail.com>
2017-05-07 10:52:47 +0200

    Nuevo tipo de pruebas

---
+++ pruebas/manuales/Prueba1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+tmux new-session -s default -n editor -d
+tmux split-window -h -t default
+tmux send-keys -t default:0.0 C-z "./main 1 2 5 1" Enter
+tmux send-keys -t default:0.1 C-z "./main 2 2 5 1" Enter
+tmux select-pane -t default:0.1
+tmux select-window -t default:0
+tmux attach-session -t default
