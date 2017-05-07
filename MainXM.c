627e8bb706b92ede042dd4f39157604b7c2242d8
ManuLG <idc.manuel@gmail.com>
2017-05-07 16:38:21 +0200

    Renombrados semáforos de paso

--- MainXM.c
+++ MainXM.c
@@ -27,26 +27,28 @@ void *hijo (void *args);
 
 // Identificadores
 int mi_id = 1;
-int mi_ticket = 0;
 int mi_prioridad = 0;
-int max_ticket = 0;
 
 // Arrays de nodos a tener en cuenta
 int id_nodos[N-1] = {2, 3};
 int id_nodos_pend[N-1];
 int num_pend = 0;
+
 // Variables para serializar
 int quiero = 0;
 int sc=0;
 int stop=0;
+
 // Colas de mesajes
 int id_cola = 0;
 int id_cola_ack = 0;
+
 // semáforos
-sem_t semH;
-sem_t semP;
+sem_t sem_paso_hijo;
+sem_t sem_paso_padre;
 
 sem_t sem_prot_quiero, sem_prot_sc, sem_prot_stop;
+
 // cantidades
 int num_hijos=0;
 int cont = 0; // Contador de hijos atendidos
@@ -68,16 +70,17 @@ int main(int argc, char const *argv[]) {
   pthread_t hiloR;
   pthread_t hilosH[2000];
 
-// Adicion mía
-  sem_init(&semH,0,0);
-  sem_init(&semP,0,0);
+
+  sem_init(&sem_paso_hijo,0,0);
+  sem_init(&sem_paso_padre,0,0);
 
   sem_init(&sem_prot_quiero, 0, 1);
   sem_init(&sem_prot_sc, 0, 1);
   sem_init(&sem_prot_stop, 0, 1);
 
   crearVector();
-  printf("ID de la cola: %i\n", id_cola);
+  printf("ID de la cola de peticiones: %i\n", id_cola);
+  printf("ID de la cola de respuestas: %i\n", id_cola_ack);
 
   // Hacer un thread para el proceso receptor
   pthread_create(&hiloR,NULL,procesoReceptor,"");
@@ -120,9 +123,9 @@ int main(int argc, char const *argv[]) {
       // ------ Inicio sección crítica ----------------
       printf("Gano la exclusión mutua\n");
     for(int i = 0;i < num_hijos;i++) {
-      sem_post(&semH);
+      sem_post(&sem_paso_hijo);
       cont++;
-      if(mi_prioridad!=4 && mi_prioridad!=5)sem_wait(&semP);
+      if(mi_prioridad!=4 && mi_prioridad!=5)sem_wait(&sem_paso_padre);
 
       sem_wait(&sem_prot_stop);
       if (stop == 1) {
@@ -160,22 +163,21 @@ void *procesoReceptor()
 {
   while(1)
   {
-    int id_nodo_origen, ticket_origen;
+    int id_nodo_origen, prioridad_origen;
 
     mensaje msg = receiveMsg(id_cola);
 
     id_nodo_origen = msg.id_nodo;
-    ticket_origen = msg.prioridad;
-    mi_ticket = mi_prioridad;
+    prioridad_origen = msg.prioridad;
 
-    if (quiero != 1 || (ticket_origen < mi_ticket && sc!=1) || (ticket_origen == mi_ticket && id_nodo_origen < mi_id && sc!=1) )
+    if (quiero != 1 || (prioridad_origen < mi_prioridad && sc!=1) || (prioridad_origen == mi_prioridad && id_nodo_origen < mi_id && sc!=1) )
     {
       sendMsg(REPLY,id_nodo_origen);
     }
     else
     {
       printf("Añadido nodo a la lista de pendientes\n" );
-      if(ticket_origen < mi_ticket && sc==1)
+      if(prioridad_origen < mi_prioridad && sc==1)
       {
         stop=1;
         printf("Recibí petición con más prioridad desde %i\n", id_nodo_origen);
@@ -249,7 +251,7 @@ void *hijo (void *arg) {
   }
 
   // Esperamos a que el padre nos de permiso (Inicio S.C)
-  sem_wait(&semH);
+  sem_wait(&sem_paso_hijo);
 
   // Imprimimos mensaje de S.C.
   switch (tipo) {
@@ -275,22 +277,22 @@ void *hijo (void *arg) {
   // Mensaje de despedida
   switch (tipo) {
 
-    case 1:  printf("Soy un proceso de pagos, y parece que ya acabé de hacer cosas.\n");
+    case 1:  printf("Soy un proceso de pagos, y salgo de mi sección crítica.\n");
              break;
-    case 2:  printf("Soy un proceso de anulaciones, y parece que ya acabé de hacer cosas.\n");
+    case 2:  printf("Soy un proceso de anulaciones, y salgo de mi sección crítica.\n");
              break;
-    case 3:  printf("Soy un proceso de pre-reservas, y parece que ya acabé de hacer cosas.\n");
+    case 3:  printf("Soy un proceso de pre-reservas, y salgo de mi sección crítica.\n");
              break;
-    case 4:  printf("Soy un proceso de gradas, y parece que ya acabé de hacer cosas.\n");
+    case 4:  printf("Soy un proceso de gradas, y salgo de mi sección crítica.\n");
              break;
-    case 5:  printf("Soy un proceso de eventos, y parece que ya acabé de hacer cosas.\n");
+    case 5:  printf("Soy un proceso de eventos, y salgo de mi sección crítica.\n");
              break;
     default: printf("Soy un proceso desconocido ...\n");
              break;
 
   }
 
-  sem_post(&semP);
+  sem_post(&sem_paso_padre);
 
   pthread_exit(NULL);
 }
