5b7f7e11fe896e5bb5b5fc1d47a827541125c37f
Xoán <xoancosmed@gmail.com>
2017-05-07 11:09:59 +0200

    Pruebas con tmux

---
+++ pruebas/manuales/Prueba4.sh
@@ -0,0 +1,9 @@
+tmux new-session -s default -n editor -d
+tmux split-window -v -t default
+tmux split-window -h -t default
+tmux send-keys -t default:0.0 C-z "./main 1 1 2 1" Enter
+tmux send-keys -t default:0.1 C-z "./main 1 2 3 1"
+tmux send-keys -t default:0.2 C-z "./main 2 2 1 1"
+tmux select-pane -t default:0.1
+tmux select-window -t default:0
+tmux attach-session -t default
---
+++ pruebas/manuales/Prueba5.sh
@@ -0,0 +1,12 @@
+tmux new-session -s default -n editor -d
+tmux split-window -h -t default
+tmux split-window -v -t default
+tmux select-pane -t default:0.0
+tmux split-window -v -t default
+tmux send-keys -t default:0.0 C-z "./main 1 3 1 1" Enter
+tmux send-keys -t default:0.1 C-z "./main 2 3 4 2" Enter
+tmux send-keys -t default:0.2 C-z "./main 2 3 5 3" Enter
+tmux send-keys -t default:0.3 C-z "./main 3 3 4 2" Enter
+tmux select-pane -t default:0.0
+tmux select-window -t default:0
+tmux attach-session -t default
