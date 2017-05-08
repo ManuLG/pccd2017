008c10b8b0460e6bcd5f04c8d6b441aaff91d8fa
Xoán Carlos Cosmed Peralejo <xoancosmed@users.noreply.github.com>
2017-05-08 11:57:12 +0200

    Update Prueba6.sh

--- pruebas/manuales/Prueba6.sh
+++ pruebas/manuales/Prueba6.sh
@@ -8,7 +8,7 @@ tmux split-window -v -t default
 tmux send-keys -t default:0.0 C-z "./main 1 4 1 1" Enter
 tmux send-keys -t default:0.1 C-z "./main 2 4 4 5" Enter
 tmux send-keys -t default:0.2 C-z "./main 3 4 5 5" Enter
-tmux send-keys -t default:0.3 C-z "./main 4 4 2 1"
+tmux send-keys -t default:0.3 C-z "./main 4 4 2 1" Enter
 tmux select-pane -t default:0.3
 tmux select-window -t default:0
 tmux attach-session -t default
