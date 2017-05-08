6de954e3c20d4fc44530a3168e1a591f13d8e5e8
Xoán Carlos Cosmed Peralejo <xoancosmed@users.noreply.github.com>
2017-05-08 10:56:29 +0200

    Update Prueba3.sh

--- pruebas/manuales/Prueba3.sh
+++ pruebas/manuales/Prueba3.sh
@@ -2,8 +2,8 @@
 
 tmux new-session -s default -n editor -d
 tmux split-window -h -t default
-tmux send-keys -t default:0.0 C-z "./main 1 1 5 1" Enter
-tmux send-keys -t default:0.1 C-z "./main 1 1 3 1"
+tmux send-keys -t default:0.0 C-z "./main 1 2 5 1" Enter
+tmux send-keys -t default:0.1 C-z "./main 2 2 3 1"
 tmux select-pane -t default:0.1
 tmux select-window -t default:0
 tmux attach-session -t default
