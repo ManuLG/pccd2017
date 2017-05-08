9552c8bab8d9dbf97adcbf020afd0d47ba5dd209
Xoán Carlos Cosmed Peralejo <xoancosmed@users.noreply.github.com>
2017-05-08 11:05:08 +0200

    Update Prueba4.sh

--- pruebas/automaticas/Prueba4.sh
+++ pruebas/automaticas/Prueba4.sh
@@ -2,14 +2,14 @@
 
 # Hay un proceso de anulaciones en la sección crítica.
 
-./main 1 1 2 1 # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
+./main 1 1 2 1 & # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
 sleep 1
 
 # Un proceso de pre-reservas (un escritor) quiere acceder a su sección crítica en el mismo nodo.
 # A la vez, un proceso de pagos en otro nodo también quiere acceder a su sección crítica.
 
-./main 1 2 3 1
-./main 2 2 1 1
+./main 1 2 3 1 &
+./main 2 2 1 1 &
 
 # Cuando el de anulaciones salga de su sección crítica, entrará el de pagos, y cuando éste salga, entrará el de pre-reservas.
 
