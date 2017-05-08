7b702c84c40b1c1ca112ea8c1f2dc6014d222e16
Xoán Carlos Cosmed Peralejo <xoancosmed@users.noreply.github.com>
2017-05-08 11:26:04 +0200

    Update Prueba6.sh

--- pruebas/automaticas/Prueba6.sh
+++ pruebas/automaticas/Prueba6.sh
@@ -2,12 +2,12 @@
 
 # Un proceso de pagos y varios lectores intentan acceder a su sección crítica.
 
-./main 1 3 1 1 # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
-./main 2 3 4 5
-./main 3 3 5 5
+./main 1 3 1 1 & # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
+./main 2 3 4 5 &
+./main 3 3 5 5 &
 sleep 2
 
 # Un proceso de anulaciones quiere acceder a su sección crítica mientras algunos lectores todavía siguen en ella.
 # Deberá esperar a que los lectores que se están ejecutando salgan de su sección crítica.
 
-./main 1 3 2 1
+./main 1 3 2 1 &
