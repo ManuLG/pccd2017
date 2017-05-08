80d230a45a0de307830ea5a0f0c8604b10db6cc1
Xoán Carlos Cosmed Peralejo <xoancosmed@users.noreply.github.com>
2017-05-08 11:01:50 +0200

    Update Prueba1.sh

--- pruebas/automaticas/Prueba1.sh
+++ pruebas/automaticas/Prueba1.sh
@@ -3,4 +3,4 @@
 # Dos procesos quieren entrar en la sección crítica a la vez estando en el mismo nodo.
 # Dado que son los únicos en nuestro sistema y que son procesos lectores, se ejecutarán concurrentemente.
 
-./main 1 1 5 2 # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
+./main 1 1 5 2 & # ID_Nodo Numero_Nodos Prioridad Numero_Hijos
